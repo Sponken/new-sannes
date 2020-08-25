a490da778d83cec9d745624bfa83c6b58d71b5b2
Li <li.ronning@gmail.com>
2020-08-25 14:45:19 +0200

    remove unused data

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-const id = 0
+
 export const testFood = {
     title: "pizza",
     price: 10,
@@ -18,26 +18,3 @@ export const pref = ["Veg", "Stark", "Inbakad"]
 
 export const groupTitles = testMenu.map(g => g.groupTitle)
 
-export const options = [{
-    text: "lök",
-    value: "lök"
-}, {
-    text: "Princess Mononoke",
-    value: "princess"
-}, {
-    text: "Aliens",
-    value: "aliens"
-}, {
-    text: "Oldboy",
-    value: "oldboy"
-}, {
-    text: "Once Upon a Time in America",
-    value: "once"
-}, {
-    text: "Witness for the Prosecution",
-    value: "Witness"
-}, {
-    text: "Das Boot",
-    value: "das",
-    disabled: true
-}];
