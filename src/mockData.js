7d36857ce458ae4967ba75d9c9500650e18fb682
Li <li.ronning@gmail.com>
2020-11-17 18:46:31 +0100

    Clean mockdata

--- src/mockData.js
+++ src/mockData.js
@@ -5,20 +5,8 @@ export const testFood = {
     price: 10,
     ingredients: ["Skinka", "lök", "jalapeño", "kyckling", "köttfärs", "bacon", "oxfilé", "mozzarellaost", "stark vitlökssås"]
 }
-const testGroup1 = {
-    groupTitle: "Testgroup",
-    foods: Array(3).fill(testFood)
-}
-const testGroup2 = {
-    groupTitle: "Testgroup2",
-    foods: Array(7).fill(testFood)
-}
-export const testMenu = [testGroup1, testGroup2]
 
 export const pref = ["Veg", "Stark", "Inbakad"]
-
-
-export const pastaMenu = [{groupTitle: "pasta", foods: foods.pastas}]
 
 let getNonPizzaGroups = () => {
     let output = []
